ca9ab3f52fac887ed0be5d9ad5e8ea4c9bd0bcda
VolodymyrSkurikhin <vladiskuri@gmail.com>
2022-08-23 14:03:12 +0300

    init updateById

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,5 +1,11 @@
 const express = require("express");
-const { add, getAll, getById } = require("../../controllers/contacts");
+const {
+  add,
+  getAll,
+  getById,
+  removeById,
+  updateById,
+} = require("../../controllers/contacts");
 
 // const Joi = require("joi");
 
@@ -42,90 +48,24 @@ router.get("/", cntrWrapper(getAll));
 
 router.get("/:contactId", isValidId, cntrWrapper(getById));
 
-// router.get("/:contactId", isValidId, async (req, res, next) => {
-//   try {
-//     const { contactId } = req.params;
-//     const oneContact = await Contact.findById(contactId);
-//     if (!oneContact) {
-//       throw RequestError(404, "Not found");
-//     }
-//     res.json(oneContact);
-//   } catch (error) {
-//     next(error);
-//   }
-// });
-
-// router.post("/", async (req, res, next) => {
-//   try {
-//     const { error } = contactSchema.validate(req.body);
-//     if (error) {
-//       throw RequestError(400, error.message);
-//     }
-//     const allContacts = await contacts.listContacts();
-//     const { name, email, phone } = req.body;
-//     const isInList = allContacts.some(
-//       (item) =>
-//         item.name === name || item.email === email || item.phone === phone
-//     );
-//     if (isInList) {
-//       throw RequestError(400, "Already in list!");
-//     }
-//     const newEntry = await contacts.addContact(req.body);
-//     res.status(201).json(newEntry);
-//   } catch (error) {
-//     next(error);
-//   }
-// });
-
-// router.post("/", async (req, res, next) => {
-//   try {
-//     const { error } = contactSchema.validate(req.body);
-//     if (error) {
-//       throw RequestError(400, error.message);
-//     }
-//     const newEntry = await Contact.create(req.body);
-//     res.status(201).json(newEntry);
-//   } catch (error) {
-//     next(error);
-//   }
-// });
-
 router.post("/", validator(contactSchema), cntrWrapper(add));
 
-// router.delete("/:contactId", async (req, res, next) => {
-//   try {
-//     const { contactId } = req.params;
-//     const result = await contacts.removeContact(contactId);
-//     if (!result) {
-//       throw RequestError(404, "Not found");
-//     }
-//     res.json({ message: "contact deleted" });
-//   } catch (error) {
-//     next(error);
-//   }
-// });
+router.delete("/:contactId", isValidId, cntrWrapper(removeById));
 
-router.delete("/:contactId", isValidId, async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    const result = await Contact.findByIdAndRemove(contactId);
-    if (!result) {
-      throw RequestError(404, "Not found");
-    }
-    res.json({ message: "contact deleted" });
-  } catch (error) {
-    next(error);
-  }
-});
+router.put("/contactId", isValidId, cntrWrapper(updateById));
 
-// router.put("/:contactId", async (req, res, next) => {
+// router.put("/:contactId", isValidId, async (req, res, next) => {
 //   try {
 //     const { error } = contactSchema.validate(req.body);
 //     if (error) {
 //       throw RequestError(400, "missing fields");
 //     }
 //     const { contactId } = req.params;
-//     const updatedContact = await contacts.updateContact(contactId, req.body);
+//     const updatedContact = await Contact.findByIdAndUpdate(
+//       contactId,
+//       req.body,
+//       { new: true }
+//     );
 //     if (!updatedContact) {
 //       throw RequestError(404, "Not found");
 //     }
@@ -134,27 +74,6 @@ router.delete("/:contactId", isValidId, async (req, res, next) => {
 //     next(error);
 //   }
 // });
-
-router.put("/:contactId", isValidId, async (req, res, next) => {
-  try {
-    const { error } = contactSchema.validate(req.body);
-    if (error) {
-      throw RequestError(400, "missing fields");
-    }
-    const { contactId } = req.params;
-    const updatedContact = await Contact.findByIdAndUpdate(
-      contactId,
-      req.body,
-      { new: true }
-    );
-    if (!updatedContact) {
-      throw RequestError(404, "Not found");
-    }
-    res.json(updatedContact);
-  } catch (error) {
-    next(error);
-  }
-});
 
 router.patch("/:contactId/favorite", isValidId, async (req, res, next) => {
   try {
