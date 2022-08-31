d7bcb3be53953127f2cb342e996e370bcc11e2cb
VolodymyrSkurikhin <vladiskuri@gmail.com>
2022-08-31 23:11:28 +0300

    finish 4 step

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,12 +1,19 @@
 const express = require("express");
-const { register, login } = require("../../controllers/auth");
+const {
+  register,
+  login,
+  logout,
+  getCurrentUser,
+} = require("../../controllers/auth");
 const { registerSchema, loginSchema } = require("../../models/users");
 const { cntrWrapper } = require("../../helpers");
-const { validator } = require("../../middlewares");
+const { validator, authenticate } = require("../../middlewares");
 
 const router = express.Router();
 
 router.post("/signup", validator(registerSchema), cntrWrapper(register));
 router.post("/login", validator(loginSchema), cntrWrapper(login));
+router.get("/logout", authenticate, cntrWrapper(logout));
+router.get("/users/current", authenticate, cntrWrapper(getCurrentUser));
 
 module.exports = router;
