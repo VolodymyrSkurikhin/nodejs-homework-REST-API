6881b57da845b623fdcb649594b8ca397f3b99e1
VolodymyrSkurikhin <vladiskuri@gmail.com>
2022-09-06 18:45:43 +0300

    fix avatar resize

--- middlewares/resizeAvatar.js
+++ middlewares/resizeAvatar.js
@@ -3,13 +3,15 @@ const path = require("path");
 
 const resizeAvatar = async (req, _, next) => {
   const { path: tempStore, filename } = req.file;
-  const tempStoreWithName = path.join(`${tempStore}`, `${filename}`);
+  // const tempStoreWithName = path.join(`${tempStore}`, `${filename}`);
   try {
-    const result = await Jimp.read(tempStoreWithName);
-    await result.resize(250, 250).write(tempStoreWithName);
+    const result = await Jimp.read(tempStore);
+    await result.resize(250, 250).write(tempStore);
+    next();
   } catch (error) {
     next(error("Avatar resize unsuccessful"));
   }
+  // next();
 };
 
 module.exports = resizeAvatar;
